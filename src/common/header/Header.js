d05be124f8d88f241cd16025463d4a1ea3dfa83a
Dipesh Singhal <dipesh.singhal@gmail.com>
2019-12-06 19:58:50 +0530

    Style initial Header

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -7,7 +7,7 @@ class Header extends Component {
    render() {
        return (
            <div className="header">
-               Image Viewer
+               <div className="title">Image Viewer</div>
            </div>
        )
    }
