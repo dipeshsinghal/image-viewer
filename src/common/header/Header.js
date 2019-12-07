617cb694cf98e6ea5906225cc16ba1b527cf9dcd
Dipesh Singhal <dipesh.singhal@gmail.com>
2019-12-07 23:08:58 +0530

    Adding search bar in header

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import './Header.css';  
+import Input from '@material-ui/core/Input';
+import SearchIcon from '@material-ui/icons/Search';
 
 class Header extends Component {
    render() {
        return (
            <div className="header">
                <div className="title">Image Viewer</div>
-           </div>
+                <div id="search-field">
+                    <div className="searchIcon" >
+                        <SearchIcon />
+                    </div>
+                    <Input className="searchInput" disableUnderline={true}  placeholder="Search..."/>
+                </div>
+            </div>
        )
    }
 }
