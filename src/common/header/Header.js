8fd8188b16044f258961969e9efdd826e2286e3a
Dipesh Singhal <dipesh.singhal@gmail.com>
2019-12-09 22:46:33 +0530

    Adding Profile Icon and menu option to it

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,22 +1,58 @@
 import React, { Component } from 'react';
-import './Header.css';  
+import './Header.css';
 import Input from '@material-ui/core/Input';
 import SearchIcon from '@material-ui/icons/Search';
+import IconButton from '@material-ui/core/IconButton';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
 class Header extends Component {
-   render() {
-       return (
-           <div className="header">
-               <div className="title">Image Viewer</div>
-                <div id="search-field">
-                    <div className="searchIcon" >
-                        <SearchIcon />
+    constructor() {
+        super();
+        this.state = {
+            anchorEl: null,
+        }
+
+    }
+    menuOpenHandler = (event) => {
+        this.setState({ anchorEl: event.currentTarget });
+        console.log("3"+this.state.anchorEl);
+    }
+    menuCloseHandler = () => {
+        console.log("1"+this.state.anchorEl);
+        this.setState({ anchorEl : null });
+        console.log("2"+this.state.anchorEl);
+    }
+    render() {
+        return (
+            <div className="header">
+                <div className="title">Image Viewer</div>
+                <div className="header-right">
+                    <div id="search-field">
+                        <div className="searchIcon" >
+                            <SearchIcon />
+                        </div>
+                        <Input className="searchInput" disableUnderline={true} placeholder="Search..." />
                     </div>
-                    <Input className="searchInput" disableUnderline={true}  placeholder="Search..."/>
+                    <IconButton id="profile-icon" edge="start" color="inherit" aria-label="menu"  >
+                        <AccountCircleIcon onClick={this.menuOpenHandler} />
+                        <Menu
+                            id="simple-menu"
+                            anchorEl={this.state.anchorEl}
+                            keepMounted
+                            open={Boolean(this.state.anchorEl)}
+                            onClose={this.menuCloseHandler}
+                        >
+                            <MenuItem onClick={this.menuCloseHandler}>My account</MenuItem>
+                            <MenuItem onClick={this.menuCloseHandler}>Logout</MenuItem>
+                        </Menu>
+                    </IconButton>
                 </div>
             </div>
-       )
-   }
+
+        )
+    }
 }
 
 export default Header;
