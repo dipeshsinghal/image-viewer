12f6d54fc74a2aa17d01b3fd91980396982e9f4b
Dipesh Singhal <dipesh.singhal@gmail.com>
2020-02-06 21:15:37 +0530

    Add Profile information on top of profile page

--- src/screens/profile/Profile.js
+++ src/screens/profile/Profile.js
@@ -3,7 +3,11 @@ import Header from '../../common/header/Header';
 import MediaGrid from '../../common/grid/MediaGrid';
 import { Container } from '@material-ui/core';
 import './Profile.css';
-
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import EditIcon from "@material-ui/icons/Edit";
+import Fab from "@material-ui/core/Fab";
 
 class Profile extends Component {
   constructor() {
@@ -55,6 +59,63 @@ class Profile extends Component {
       <div>
         <Header {...this.props} showSearchBar={false} showProfileIcon={true} showOnlyLogoutMenu={true} />
         <Container>
+        <div style={{ height: "2rem" }}></div>
+        <Grid container spacing={3} justify="flex-start">
+            <Grid item xs={2}/>
+            <Grid item xs={2}>
+              {this.state.userProfileData ? (
+                <Avatar
+                  alt={this.state.userProfileData.full_name}
+                  id="profile-image"
+                  fontSize="small"
+                  variant="circle"
+                  src={this.state.userProfileData.profile_picture}
+                />
+              ) : null}
+            </Grid>
+            <Grid item xs={4}>
+              <Typography variant="h5" component="h2">
+                {this.state.userProfileData
+                  ? this.state.userProfileData.username
+                  : null}
+              </Typography>
+              <Grid container spacing={3} justify="center">
+                <Grid item xs={4}>
+                  Posts:{" "}
+                  {this.state.userProfileData
+                    ? this.state.userProfileData.counts.media
+                    : null}
+                </Grid>
+                <Grid item xs={4}>
+                  Follows:{" "}
+                  {this.state.userProfileData
+                    ? this.state.userProfileData.counts.follows
+                    : null}
+                </Grid>
+                <Grid item xs={4}>
+                  Follows By:{" "}
+                  {this.state.userProfileData
+                    ? this.state.userProfileData.counts.followed_by
+                    : null}
+                </Grid>
+              </Grid>
+              <Typography variant="h5" component="h2">
+                {this.state.fullname ? this.state.fullname : null}
+                {this.state.userProfileData && !this.state.fullname
+                  ? this.state.userProfileData.full_name
+                  : null}
+                <Fab
+                  color="secondary"
+                  id="edit-profile"
+                  aria-label="edit"
+                  onClick={this.handleOpen}
+                >
+                <EditIcon fontSize="large" />
+                </Fab>
+              </Typography>
+            </Grid>
+            <Grid item xs={4}/>
+          </Grid>
           <MediaGrid {...this.props}  userRecentMediaData={this.state.userRecentMediaData} />
         </Container>
       </div>
